48f400d041b94873455962b6120733461c789dea
Marco <markoka1@outlook.it>
2020-10-28 15:51:04 +0100

    esercizio completo

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
         // recupero l'immagine corrente
         var img_corrente = $('img.active');
         // recupero il pallino corrente
-        var pallino_corrente = $('indicatori_immagine i');
+        var pallino_corrente = $('.indicatori_immagine i.fas');
+        //var pallino_corrente = $('.indicatori_immagine i.fas');
 
         // tolgo la classe active all'immagine corrente
         img_corrente.removeClass('active');
@@ -21,7 +22,7 @@ $(document).ready(function() {
             img_corrente.next('img').addClass('active');
 
             // assegno la classe fas al pallino successivo
-            pallino_corrente.next('indicatori_immagine i').addClass('fas');
+            pallino_corrente.next('.indicatori_immagine i').removeClass('far').addClass('fas');
         } else {
             // non c'è un'immagine successiva
             // assegno la classe active alla prima immagine
@@ -37,26 +38,26 @@ $(document).ready(function() {
         // recupero l'immagine corrente
         var img_corrente = $('img.active');
         // recupero il pallino corrente
-        var pallino_corrente = $('span.active');
-
+        var pallino_corrente = $('.indicatori_immagine i.fas');
         // tolgo la classe active all'immagine corrente
         img_corrente.removeClass('active');
-        // tolgo la classe active al pallino corrente
-        pallino_corrente.removeClass('active');
+        // tolgo la classe fas al pallino corrente
+        pallino_corrente.removeClass('fas').addClass('far');
 
         // verifico se esiste un'immagine precedente
         if(img_corrente.prev('img').length) {
-            // c'è un'immagine precedente
-            // assegno la classe active all'immagine precedente
+            // c'è un elemento successivo
+            // assegno la classe active all'immagine successiva
             img_corrente.prev('img').addClass('active');
-            // assegno la classe active al pallino precendete
-            pallino_corrente.prev('span').addClass('active');
+
+            // assegno la classe fas al pallino successivo
+            pallino_corrente.prev('.indicatori_immagine i').removeClass('far').addClass('fas');
         } else {
             // non c'è un elemento precedente
             // assegno la classe active all'ultima immagine
             $('.carousel img:last-of-type').addClass('active');
             // assegno la classe active all'ultimo pallino
-            $('.indicatori_immagine span:last-child').addClass('active');
+            $('.indicatori_immagine i:last-child').removeClass('far').addClass('fas');
         }
     });
 
